a96fc9a5b8ab0d4d60c1882e85d0d1f032e425cc
Fzocchi10 <fzocchi97@gmail.com>
2025-05-12 14:33:03 -0300

    Mejore los controller para no mezclar los servicios

--- src/Controllers/autoController.ts
+++ src/Controllers/autoController.ts
@@ -18,8 +18,7 @@ export const getAutos = async (req: Request, res: Response) => {
 export const getAuto = async (req: Request, res: Response): Promise<void> => {
     const { id }  = req.params;
     const auto = await autosService.getById(id) as Auto;
-    let duenioId = await (req as any).auto.duenioId as string;
-    const persona = await personaService.getById(duenioId) as Persona;
+    const persona = await personaService.getById(auto.duenioId) as Persona;
     res.status(200).json({
         "marca":auto.marca,
         "modelo": auto.modelo,
@@ -36,10 +35,8 @@ export const getAuto = async (req: Request, res: Response): Promise<void> => {
 export const postAuto = async (req: Request, res: Response) => {
     const data = req.body;
     const duenioExistente = (req as any).duenio as Persona;
-    const idDuenio = duenioExistente.id as string;
 
-    const nuevoAuto = await autosService.create(idDuenio,data);
-    personaService.addAuto(idDuenio, nuevoAuto);
+    const nuevoAuto = await autosService.create(duenioExistente.id,data);
     res.status(200).json({ id: nuevoAuto });
 };
 
@@ -49,14 +46,13 @@ export const putAuto = (req: Request, res: Response) => {
     const data = req.body;
 
     autosService.update(id,data);
-
     res.status(200).json({ mensaje: 'Auto actualizado correctamente'});
-
 };
 
 // Eliminar un auto
 export const deleteAuto = (req: Request, res: Response) => {
     const id = req.params.id as UUID;
     autosService.delete(id);
+    res.status(200).json({ mensaje: 'Auto eliminado correctamente'});
 };
 
