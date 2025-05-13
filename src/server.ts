48b46b94b19baf911ae0bc406c0259407d185efa
Fzocchi10 <fzocchi97@gmail.com>
2025-05-13 10:40:14 -0300

    Se agrego la conexion a mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,28 @@
 import process from 'process';
 import app from './app';
+const { MongoClient } = require('mongodb');
+
+const url = process.env.DATABASE;
+const client = new MongoClient(url);
+const dbName = process.env.DATABASE_NAME;
 
 const port = process.env.PORT || 9000;
+
+if(process.env.DATABASE_TYPE === "memory"){
+  console.log('Modo memoria activado');
+} else {
+  async function main() {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    return 'done.';
+  }
+  main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+}
+
 
 app.get
   app.listen(port, () => {
